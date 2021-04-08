4e52fa2a6c9899b4a50b36119107251f7f43a840
Alberto <alosadag@katatonic.mirhome.local>
2021-04-08 13:26:07 +0200

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,12 @@ REPOURL="https://github.com/alosadagrande/laptop_install.git"
 GITDIR="${HOME}/git"
 GITREPO="${GITDIR}/laptop_install"
 
-sudo dnf install -y ansible git dnf-plugins-core libselinux-python
+if [ -d "${GITREPO}" ];then
+ echo "Folder exists, deleting..."
+ rm -rf ${GITREPO}
+fi
+
+sudo dnf install -y ansible git dnf-plugins-core 
 
 mkdir -p ${GITDIR}
 
